4e20eb4876d93b4bc01e19b92edf3d402b36f4cd
majinmaru <gotch11@naver.com>
2022-02-20 23:05:57 +0900

    220220

---
+++ promotion/javascripts/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.cards').slick({
+		infinite: true,
+	    slidesToShow: 4,
+	    prevArrow: $('.button.back'),
+	    nextArrow: $('.button.next')
+	});
+
+	$('.button.back').hover(function(){
+		$('.back img').attr("src","../common/images/button-back_active.png")
+	}, function(){
+		$('.back img').attr("src","../common/images/button-back.png")
+	})
+	$('.button.next').hover(function(){
+		$('.next img').attr("src","../common/images/button-next_active.png")
+	}, function(){
+		$('.next img').attr("src","../common/images/button-next.png")
+	})
+});
