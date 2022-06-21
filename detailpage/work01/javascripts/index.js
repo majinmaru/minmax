ad2314ae047ea945b11c296e2f6e7b2c38fe9638
majinmaru <gotch11@naver.com>
2022-06-22 00:01:42 +0900

    220622

---
+++ detailpage/work01/javascripts/index.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	/*$('.content-btn').click(function(e){
+		e.preventDefault();
+		var currentIndex = $(this).index();
+		$('.content-btn').eq(currentIndex).addClass('on');
+		$('.content-btn').eq(currentIndex).siblings().removeClass('on');
+
+		$('.img-promo').eq(currentIndex).addClass('on');
+		$('.img-promo').eq(currentIndex).siblings().removeClass('on');
+	});
+	*/
+	
+	var floatingBtn = function() {
+		var contentsTop = $(".warpper-content").offset().top;
+		var scrollTop = $(window).scrollTop();
+		if(scrollTop > contentsTop){
+			$(".floating").addClass("on");
+		} else {
+			$(".floating").removeClass("on");
+		} 
+	}
+	floatingBtn();
+
+	
+	$('.floating-close').click(function(e){
+		e.preventDefault();
+		if($('.floating').hasClass('min')){
+			$('.floating').removeClass('min');
+			$('.floating-btn').removeClass('min');
+			$('.floating-close').removeClass('min');
+		} else {
+			$('.floating').addClass('min');
+			$('.floating-btn').addClass('min');
+			$('.floating-close').addClass('min');
+		}
+	});
+
+
+	$('.floating-btn').click(function(e){
+		e.preventDefault();
+		var contentsTop = $(".warpper-content").offset().top;
+		var headerHeight = $("#header").height();
+		$("html, body").animate({
+			scrollTop: contentsTop - headerHeight
+		}, 400)
+	});
+
+	$(window).scroll(function() {
+		floatingBtn();
+	});
+});
