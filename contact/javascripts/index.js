5a0e92b9e1be4c552c23f586be16ad43c461ee11
majinmaru <gotch11@naver.com>
2022-07-26 00:26:22 +0900

    contact
    added

--- contact/javascripts/index.js
+++ contact/javascripts/index.js
@@ -1,2 +1,9 @@
 $(document).ready(function(){
+	$(function(){
+		$(".introduce-email").click(function(){
+			$(".introduce-emailtxt").select();
+			document.execCommand("copy");
+			alert("클립보드에 복사되었습니다");
+		});
+	});
 });
