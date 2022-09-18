5d2fbad9a6ab27ae1f08a071dd39c7927ba93a9f
majinmaru <gotch11@naver.com>
2022-09-19 00:13:11 +0900

    220919
    main updated

--- main/javascripts/index.js
+++ main/javascripts/index.js
@@ -1,2 +1,47 @@
 $(document).ready(function(){
+	$('.content-slider.project').slick({
+		infinite: true,
+	    slidesToShow: 2,
+	    prevArrow: $('.project.back'),
+	    nextArrow: $('.project.next'),
+	    variableWidth: true,
+	    responsive: [
+    		{
+      			breakpoint: 768,
+      			settings: {
+        		slidesToShow: 1
+        		}
+        	}
+    	]
+	});
+	$('.content-slider.promotion').slick({
+		infinite: true,
+	    slidesToShow: 2,
+	    prevArrow: $('.promotion.back'),
+	    nextArrow: $('.promotion.next'),
+	    variableWidth: true,
+	    responsive: [
+    		{
+      			breakpoint: 768,
+      			settings: {
+        		slidesToShow: 1
+        		}
+        	}
+    	]
+	});
+	$('.content-slider.detailpage').slick({
+		infinite: true,
+	    slidesToShow: 2,
+	    prevArrow: $('.detailpage.back'),
+	    nextArrow: $('.detailpage.next'),
+	    variableWidth: true,
+	    responsive: [
+    		{
+      			breakpoint: 768,
+      			settings: {
+        		slidesToShow: 1
+        		}
+        	}
+    	]
+	});
 });
